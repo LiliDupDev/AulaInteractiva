119f621f8362c0358342406bcf717b66aaa52fc0
LiliDupDev <lilydup@outlook.com>
2020-02-28 14:48:24 -0600

    Agrega sensores

--- Observer.py
+++ Observer.py
@@ -1,17 +1,16 @@
 from Sockets.SocketClient import SocketClient
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 
 
 class Observer(SocketClient):
-    """mpin = 17
+    mpin = 17
     tpin = 27
     cap = 0.000001
     adj = 2.130620985
     i = 0
     t = 0
-
     def LDR(self):
         try:
             lecture=0 
@@ -38,16 +37,9 @@ class Observer(SocketClient):
             return lecture
         except:
             GPIO.cleanup()
-        """
+        
     def __init__(self,ip,port):
         SocketClient.__init__(self,ip,port)
-
-    def run2(self,message):
-        self.open_connection()
-        self.send_message(bytes(message, 'UTF-8'))
-        self.close_connection()
-
-
 
     def run(self):
         self.open_connection()
@@ -58,7 +50,7 @@ class Observer(SocketClient):
         while(True):
             #in_data = self.receive_package(1024)
             #print("From Server :", in_data.decode())
-            out_data = "1"#str(self.LDR())
+            out_data = str(self.LDR())
             print("Data", out_data)
             if out_data!='0':
                 self.send_message(bytes(out_data, 'UTF-8'))
@@ -68,9 +60,9 @@ class Observer(SocketClient):
         
             
 
-obs=Observer("192.168.0.139",10000)
-#obs = Observer("192.168.0.139", 8080)
-#GPIO.setmode(GPIO.BCM)
-obs.run2("LILI CHAN")
-#GPIO.cleanup()
+
+obs = Observer("192.168.0.144", 8080)
+GPIO.setmode(GPIO.BCM)
+obs.run()
+GPIO.cleanup()  
 		
--- Server.py
+++ Server.py
@@ -1,5 +1,6 @@
 from Sockets.SocketServer import SocketServer
-import Observer
+import RPi.GPIO as GPIO
+import time
 
 class Server(SocketServer):
 
@@ -11,26 +12,25 @@ class Server(SocketServer):
         self.open_connection()
         print("Connected client :", self.clientAddress)
         msg = ''
-
         while True:
             in_data = self.receive_package(1024)
             msg = in_data.decode()
-            #print(msg)
-            try:
-                value = int(msg)
-                if value > 10000:
-                    obs = Observer("192.168.0.139", "10000")
-                    obs.run2('0')
-            except:
-                print(msg)
-            #self.send_message(bytes(str(value), 'UTF-8'))
-
+            print(msg,',')
+            if msg == '1':
+                GPIO.output(18, GPIO.HIGH)
+            if msg == '0':
+                GPIO.output(18, GPIO.LOW)
+            if msg == 'bye':
+                break
             #print("From Client :", msg)
             #out_data = input()
             #self.send_message(bytes(out_data, 'UTF-8'))
-        #print("Client disconnected...")
+
+        print("Client disconnected...")
         self.close_connection()
 
-
-serv = Server("192.168.0.145", 8080)
+GPIO.setmode(GPIO.BCM)
+serv = Server("192.168.0.139",10000)
+GPIO.setup(18,GPIO.OUT)
 serv.listen_connection()
+GPIO.cleanup()
