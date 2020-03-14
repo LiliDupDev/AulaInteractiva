508a17820a03aef0839be3ccca1f7faac0e59cbc
Liliana <55676248+LiliDupDev@users.noreply.github.com>
2020-03-13 18:00:05 -0600

    Comments

--- RMI_Thermostater.py
+++ RMI_Thermostater.py
@@ -1,8 +1,11 @@
 import Pyro4
 
+# Remote object
 @Pyro4.expose
 class Thermostater(object):
 
+    #Based on the argument sent it calculates the right temperature
+    # TODO: return must be compatible with an air conditioner
     def evaluateTemperature(self,temperature):
         if temperature < 20:
             a = 'La temperatura esta baja, lo recomendado es entre 20°C y 25°C'
@@ -11,20 +14,21 @@ class Thermostater(object):
             a = 'La temperatura esta alta, lo recomendado es entre 20°C y 25°C'
             return a
         elif temperature >= 20 & temperature <= 25:
-            a = 'Esta bien k'
+            a = 'Esta bien'
             return a
 
+    # Start the background process that will have the remote object
     def start_server(self):
         daemon = Pyro4.Daemon()
-        ns = Pyro4.locateNS()
-        uri = daemon.register(Thermostater)
-        ns.register('thermostater.server', str(uri))
+        ns = Pyro4.locateNS()                       # Locate name server
+        uri = daemon.register(Thermostater)         # Create the identifier for the remote object
+        ns.register('thermostater.server', str(uri))# Assing a name for the remote object
         print(f'Ready to listen')
         print(uri)
-        daemon.requestLoop()
+        daemon.requestLoop()                        # Leave the daemon running
 
 
-
+# Connection string, this is the server connection and the port
 Pyro4.Daemon.serveSimple({Thermostater: 'thermostater.server', }, host="192.168.0.144", port=50000, ns=False,
                             verbose=True)
 if __name__ == '__main__':
--- Thermostat_control.py
+++ Thermostat_control.py
@@ -1,18 +1,20 @@
 import Pyro4
 import serial
 
+#This script monitor the temperature sensor and call the remote object Thermostater to evaluate temperature
 # ns = Pyro4.locateNS()
 # uri=ns.lookup('thermostat')
 # server = Pyro4.Proxy(uri)
 
+# Server address
 server = Pyro4.Proxy("PYRO:thermostater.server@192.168.100.150:50000")
+
 if __name__ == '__main__':
     try:
-        arduino = serial.Serial('COM3', baudrate=9600, timeout=1.0)
+        arduino = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=1.0)
         while True:
-            temperatura = (arduino.readline()).decode()
-            #print(temperatura)
-            print(server.evaluateTemperature(26))
+            temp = (arduino.readline()).decode()
+            print(server.evaluateTemperature(temp))
     except (KeyboardInterrupt, EOFError):
         print('Goodbye! (:')
 exit
